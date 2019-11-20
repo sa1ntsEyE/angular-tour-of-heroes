62b3ebe223c7991852d2672c2056693507724f30
Alex Kazarinov <stolbnyak2003@gmail.com>
2019-11-20 21:37:24 +0300

    Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+
 
 @Component({
   selector: 'app-heroes',
@@ -26,3 +28,5 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 }
+
+
