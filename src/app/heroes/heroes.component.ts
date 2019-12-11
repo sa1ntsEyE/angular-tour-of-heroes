774755fc9ad9984ac4371a7c3e5e225c65686634
Alex Kazarinov <stolbnyak2003@gmail.com>
2019-12-11 17:18:14 +0300

    NOW

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+
 import { HeroService } from '../hero.service';
-
-
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
 
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] = [];
   selectedHero: Hero;
 
-  constructor() {
-  }
+  constructor() { }
 
   ngOnInit() {
   }
@@ -28,7 +23,5 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 }
-
-
 
 
